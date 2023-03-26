a551e44df4dccc8ebc44724c24f1f74c618a194c
Rajwol <rajwolchapagain@gmail.com>
2023-03-25 20:40:35 -0400

    replace bullet sprite. rework gun rotation and shoot mechanics

--- player/robot.gd
+++ player/robot.gd
@@ -4,11 +4,18 @@ const MOVE_SPEED = 400
 const JUMP_SPEED = 700
 var is_grounded = false
 var facing_right = true
+var distance_to_gun
+var increment_angle_per_second_degrees = 100
+var gun_rotation_angle = 0
+var sensitivity = 5
 
-func _process(delta):
+func _ready():
 	if $Gun != null:
-		$Gun.look_at(get_global_mouse_position())
-		
+		distance_to_gun = $Gun.position.length()
+
+func _physics_process(delta):
+		handle_gun_rotation(delta)
+	
 func _integrate_forces(state):
 	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
 		linear_velocity.x = - MOVE_SPEED
@@ -37,3 +44,20 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if "platform" in body.get_groups():
 		is_grounded = false
+
+func handle_gun_rotation(physics_process_delta):
+	if Input.is_action_pressed("rotate_gun_clockwise"):
+		gun_rotation_angle += increment_angle_per_second_degrees * physics_process_delta * sensitivity
+	if Input.is_action_pressed("rotate_gun_counter_clockwise"):
+		gun_rotation_angle -= increment_angle_per_second_degrees * physics_process_delta * sensitivity
+	
+	if $Gun.position.x < 0:
+		$Gun/Sprite2D.flip_v = true
+	else:
+		$Gun/Sprite2D.flip_v = false
+		
+	$Gun.position.x = distance_to_gun * cos(deg_to_rad(gun_rotation_angle))
+	$Gun.position.y = distance_to_gun * sin(deg_to_rad(gun_rotation_angle))
+	
+	$Gun.rotation_degrees = gun_rotation_angle
+	
