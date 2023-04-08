c0bac08eb18763d195cb8cb0d26cab4add5f2a3b
Rajwol <rajwolchapagain@gmail.com>
2023-04-07 21:38:08 -0400

    fix spawnrate

--- enemy/enemy_spawner.gd
+++ enemy/enemy_spawner.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 const ENEMY_SCENE = preload("res://enemy/enemy.tscn")
+const MAX_ENEMY_PER_SECOND = 1
 var spawn_rate = 0.2
-var spawn_rate_increment = 0.1
+var spawn_rate_increment = 0.05
 var max_x_coordinate_spawn
 var camera_zoom = 0.8 #get this from the main scene's camera
 var spawn_y_offset = 200
@@ -23,6 +24,7 @@ func _on_spawn_rate_timer_timeout():
 
 func _on_rate_increment_timer_timeout():
 	spawn_rate += spawn_rate_increment
+	spawn_rate = clamp(spawn_rate, 0, MAX_ENEMY_PER_SECOND)
 	set_timer_wait_time()
 
 func pick_random_spawn_position():
