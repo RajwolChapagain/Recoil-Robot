f3ddc7fb2a81c2f14ae8d4c1239ef6ac36506d86
Rajwol <rajwolchapagain@gmail.com>
2023-04-25 14:46:08 -0400

    implement bomb radius and tweak z index ordering of scenes

--- enemy/bomb.gd
+++ enemy/bomb.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var explosion_force = 1000
+var explosion_radius_display_duration = 0.8
 
 func _on_body_entered(body):
 		explode()
@@ -8,5 +9,14 @@ func _on_body_entered(body):
 func explode():
 	for body in $ExplosionArea.get_overlapping_bodies():
 		if body.is_in_group("enemy") or body.name == "Robot":
-			body.apply_central_impulse(explosion_force * global_position.direction_to(body.global_position))	
+			body.apply_central_impulse(explosion_force * global_position.direction_to(body.global_position))
+	
+	$Sprite2D.visible = false
+	$BombRadiusSprite.visible = true
+	call_deferred("freeze_body_and_disable_collision_shape")
+	await get_tree().create_timer(explosion_radius_display_duration).timeout
 	queue_free()
+
+func freeze_body_and_disable_collision_shape():
+	freeze = true
+	$BombCollisionShape.disabled = true
