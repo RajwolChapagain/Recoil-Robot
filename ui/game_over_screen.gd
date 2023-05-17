9020ea1be75455d44ef34ae55abeab64ce8d8d7d
Rajwol <rajwolchapagain@gmail.com>
2023-05-17 07:13:36 -0400

    change game over panel layout

--- ui/game_over_screen.gd
+++ ui/game_over_screen.gd
@@ -5,7 +5,7 @@ func _on_restart_button_button_down():
 
 func display_game_over_panel(kills, time):
 	visible = true
-	$%Kills.text += str(kills)
+	$%Kills.text += " " + str(kills)
 	#$%Time.text += str(time)
 	
 		
@@ -13,7 +13,7 @@ func display_game_over_panel(kills, time):
 		save_kills_and_time(kills, time)
 		
 	var high_scores = load_kills_and_time()
-	$%BestKills.text += str(high_scores["kills"])
+	$%BestKills.text += " " + str(high_scores["kills"])
 	#$%AccompanyingTime.text += str(high_scores["time"])
 
 func save_kills_and_time(kills, time):
