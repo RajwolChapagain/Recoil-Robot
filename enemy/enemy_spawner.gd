20328ccb30650906a709aff620e108911055780c
Rajwol <rajwolchapagain@gmail.com>
2023-06-27 23:28:24 -0400

    improve enemy spawning by implementing wave system

--- enemy/enemy_spawner.gd
+++ enemy/enemy_spawner.gd
@@ -9,32 +9,18 @@ var camera_zoom = 0.8 #get this from the main scene's camera
 var spawn_y_offset = 200
 var rate_increase_interval = 20
 
+var interval_between_waves = 5
+var current_minimum_enemy_count = 1
+var maximum_minimum_enemy_count = 10
+var current_maximum_enemy_count = 1
+var interval_between_enemies = 0.5
+var enemy_count_in_current_wave = 1
+var enemies_spawned_this_wave = 0
+var minimum_time_between_enemies = 1
+var maximum_time_between_enemies = 3
+
 signal enemy_spawned(enemy)
 
-func _ready():
-	$RateIncrementTimer.wait_time = rate_increase_interval
-	set_timer_wait_time()
-
-func set_timer_wait_time():
-	$SpawnRateTimer.wait_time = 1 / float(spawn_rate)
-	
-func _on_spawn_rate_timer_timeout():
-	var enemy = ENEMY_SCENE.instantiate()
-	add_child(enemy)
-	enemy.global_position = pick_random_spawn_position()
-	emit_signal("enemy_spawned", enemy)
-
-func _on_rate_increment_timer_timeout():
-	spawn_rate += spawn_rate_increment
-	spawn_rate = clamp(spawn_rate, 0, MAX_ENEMY_PER_SECOND)
-	
-	if spawn_rate >= MAX_ENEMY_PER_SECOND:
-		stop_incrementing_rate()
-		$IncreaseLabel.text = "Max Spawnrate Reached!"
-		
-	$IncreaseLabel.visible = true
-	$IncreaseLabel/LabelAppearTimer.start()
-	set_timer_wait_time()
 
 func pick_random_spawn_position():
 	var x = randi_range(-240, max_x_coordinate_spawn)
@@ -42,13 +28,42 @@ func pick_random_spawn_position():
 	
 	return Vector2(x, y)
 
-
-func _on_label_appear_timer_timeout():
-	$IncreaseLabel.visible = false
 	
 func stop_spawning():
-	$SpawnRateTimer.stop()
-	stop_incrementing_rate()
+	$WaveIntervalTimer.stop()
+	$EnemySpawnTimer.stop()
 
-func stop_incrementing_rate():
-	$RateIncrementTimer.stop()
+func _on_wave_interval_timer_timeout():
+	start_wave()
+
+func start_wave():
+	enemies_spawned_this_wave = 0
+	enemy_count_in_current_wave = randi_range(current_minimum_enemy_count, current_maximum_enemy_count)
+	print(enemy_count_in_current_wave)
+	spawn_enemy()
+	$EnemySpawnTimer.start()
+	
+func end_wave():
+	$EnemySpawnTimer.stop()
+	enemies_spawned_this_wave = 0
+	if current_minimum_enemy_count != maximum_minimum_enemy_count:
+		if current_minimum_enemy_count == current_maximum_enemy_count:
+			current_maximum_enemy_count *= 2
+		current_minimum_enemy_count += 1
+	$WaveIntervalTimer.start()
+		
+func spawn_enemy():
+	if enemies_spawned_this_wave == enemy_count_in_current_wave:
+		end_wave()
+		return
+		
+	var enemy = ENEMY_SCENE.instantiate()
+	add_child(enemy)
+	enemy.global_position = pick_random_spawn_position()
+	emit_signal("enemy_spawned", enemy)
+	enemies_spawned_this_wave += 1
+	$EnemySpawnTimer.wait_time = randf_range(minimum_time_between_enemies, maximum_time_between_enemies)
+	$EnemySpawnTimer.start()
+
+func _on_enemy_spawn_timer_timeout():
+	spawn_enemy()
